c778f339e61054563dfea5f8fbfc661a043fabce
Jonathan Arrance <jonathan.arrance@gmail.com>
2023-08-24 13:49:04 -0400

    Checkpoint
    Last update before preso.

--- setup_robusta.sh
+++ setup_robusta.sh
@@ -5,15 +5,19 @@
 minikube start
 
 wait $!
+sleep 2
 minikube addons enable ingress
 
 wait $!
+sleep 2
 minikube addons enable metrics-server
 
-wait $!
-helm repo add robusta https://robusta-charts.storage.googleapis.com
+#wait $!
+#sleep 2
+#helm repo add robusta https://robusta-charts.storage.googleapis.com
 
 wait $!
+sleep 5
 #Keep the values file out of your root since it has sec keys in it. Do not load into github.
 helm install robusta robusta/robusta -f ../generated_values.yaml --set clusterName=PromBot
 
--- stop_robusta.sh
+++ stop_robusta.sh
@@ -2,10 +2,12 @@
 
 kubectl -n default delete svc/robusta-grafana
 
+sleep 1
+
 helm uninstall robusta robusta/robusta
 
 wait $!
-
+sleep 5
 minikube stop
 
 echo "Dev testbed shut down"
